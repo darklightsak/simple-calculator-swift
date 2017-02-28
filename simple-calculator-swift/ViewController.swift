5732e32df7234043148f5d4b177a8392e7487c45
Surasak Wattanapradit <s.wattanapradit@gmail.com>
2017-02-28 23:51:05 +0700

    Create Project
    First! Create Project.

---
+++ simple-calculator-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-calculator-swift
+//
+//  Created by Surasak Wattanapradit on 2/28/2560 BE.
+//  Copyright © 2560 Surasak Wattanapradit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
